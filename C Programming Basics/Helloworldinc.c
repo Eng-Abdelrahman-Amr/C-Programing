a0f2b13a8ab37f087315c09983cdaebf018844ac
Eng-Abdelrahman-Amr <75952980+Eng-Abdelrahman-Amr@users.noreply.github.com>
2021-02-06 16:00:59 +0200

    Add files via upload

---
+++ C Programming Basics/Helloworldinc.c
@@ -0,0 +1,26 @@
+
+/*
+
+	multi-line comment
+	include standard input output library
+	to use printf function
+	
+*/
+
+#include<stdio.h>
+
+//define main function (this is my entry point of my program)
+
+void main (void)
+{
+	
+	/*
+		call printf function and passing string to it
+		to print it on cmd
+	*/
+	
+	printf("\"Hello World In C\"\n");
+	
+	printf("Hello World \tIn \'C\'");
+	
+}
---
+++ C Programming Basics/Lab1.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+
+
+void main (void)
+{
+	
+
+	
+	printf("\"I\'m \"Abdelrahman Amr\"\n");
+	
+	printf("My birthday date is 19 Aug 1999\n");
+	printf("I\'m  student at Faculty of Engineering\n\"Al-Azhar University\"");
+	
+}
